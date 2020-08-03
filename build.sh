384eb8ed226c8673548a075be4f73fa97f69d426
Conlin Durbin <conlin@hackerrank.com>
2020-08-03 13:52:45 -0400

    Move book to subdirectory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p output
-pandoc -f gfm --lua-filter plugins/pagebreak.lua -o output/ClearCode.epub --metadata-file=metadata.yml --metadata title="wuz's guide to clear code" --toc -s *.md
-pandoc -f gfm --lua-filter plugins/pagebreak.lua --pdf-engine=wkhtmltopdf --css assets/pdf.css -o output/ClearCode.pdf --metadata title="wuz's guide to clear code" --toc -s *.md
+pandoc -f gfm --lua-filter plugins/pagebreak.lua -o output/ClearCode.epub --metadata-file=metadata.yml --metadata title="wuz's guide to clear code" --toc -s book/*.md
+pandoc -f gfm --lua-filter plugins/pagebreak.lua --pdf-engine=wkhtmltopdf --css assets/pdf.css -o output/ClearCode.pdf --metadata title="wuz's guide to clear code" --toc -s book/*.md
